3207ff21c063bd96ecb19e6436b32dc1967d1907
truman.eustace <true92410@gmail.com>
2026-01-07 20:48:10 -0500

    this is for another day

--- TeamCode/src/main/java/ScheduleExecutorTest.java
+++ TeamCode/src/main/java/ScheduleExecutorTest.java
@@ -1,14 +1,6 @@
-import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.VoltageSensor;
-
-import org.firstinspires.ftc.robotcore.internal.camera.delegating.DelegatingCaptureSequence;
-import org.firstinspires.ftc.teamcode.util.Debouncer;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 //this is the final one
 
@@ -19,6 +11,9 @@ public class ScheduleExecutorTest extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
+        waitForStart();
+        ScheduledExecutorService executorService = Executors.newSingleThreadExecutor();
+        Runnable runnableTask = () -> System.out.println("Task is running: " + System.currentTimeMillis());
 
     }
 }
