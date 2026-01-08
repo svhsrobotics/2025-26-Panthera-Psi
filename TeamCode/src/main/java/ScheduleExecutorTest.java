f3cd752e27d68690e07d1e605699fda546665ff4
truman.eustace <true92410@gmail.com>
2026-01-07 19:57:27 -0500

    i'm going to try something new

---
+++ TeamCode/src/main/java/ScheduleExecutorTest.java
@@ -0,0 +1,24 @@
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
+
+import org.firstinspires.ftc.robotcore.internal.camera.delegating.DelegatingCaptureSequence;
+import org.firstinspires.ftc.teamcode.util.Debouncer;
+
+//this is the final one
+
+//unless gradle KILLS ITSELF again
+
+public class ScheduleExecutorTest extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
